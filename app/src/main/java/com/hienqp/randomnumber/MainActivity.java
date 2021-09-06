e598f9146fab289787db626b080714857f1ef526
hienqp <hienphan5290@gmail.com>
2021-09-07 01:33:27 +0700

    RandomNumber

---
+++ app/src/main/java/com/hienqp/randomnumber/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hienqp.randomnumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtNumber;
+    Button btnRandom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNumber = (TextView) findViewById(R.id.textViewNumber);
+        btnRandom = (Button) findViewById(R.id.buttonRandom);
+
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int number = random.nextInt(5); // [0 : (5-1)]
+
+                txtNumber.setText(String.valueOf(number));
+            }
+        });
+    }
+}
